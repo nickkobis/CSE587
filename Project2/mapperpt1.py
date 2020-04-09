d146e205241b679127711c830bd4e4e549dce210
Nick Kobis <nickkobis@gmail.com>
2020-04-09 14:23:33 -0400

    correctly did the mapper and reducer

--- Project2/mapperpt1.py
+++ Project2/mapperpt1.py
@@ -1,8 +1,15 @@
 import sys
-
+import string
+punctuation = string.punctuation
 for data in sys.stdin:
-    data = data.strip()
+    data = data.lower()
+    list_of_chars = list(data)
+    for char in list_of_chars:
+        if punctuation.__contains__(char):
+            char = " "
+    new_string = ''.join(list_of_chars)
     #words is a list
-    words = data.split() 
+    words = new_string.split() 
     for word in words:
+
         print(word, "\t1")
