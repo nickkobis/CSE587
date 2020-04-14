435f5ff4b0c007c4348e7c3551d4ec82af92a070
ryan <ryanszcz@buffalo.edu>
2020-04-13 21:00:26 -0400

    finished part 2
    run with make run2

---
+++ Project2/part2/mapperpt2.py
@@ -0,0 +1,22 @@
+import sys
+import string
+punctuation = string.punctuation
+special = ["science", "sea","fire"]
+for data in sys.stdin:
+    data = data.lower()
+    words = data.split()
+    avalible = [1,1,1]
+    length= len(words)
+    for i in range(length):
+        if words[i] in special:
+            if i+2 < length:
+                word = words[i] + "_" + words[i+1] + "_" + words[i+2]
+                print(word, "\t1")
+            if i+1 < length and i-1 >= 0:
+                word = words[i-1] + "_" + words[i] + "_" + words[i+1]
+                print(word, "\t1")
+            if i-2 >= 0:
+                word = words[i-2] + "_" + words[i-1] + "_" + words[i]
+                print(word, "\t1")
+
+        
