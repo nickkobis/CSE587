5fb04a032020f497670672abe79bd8712d71c580
Nick Kobis <nickkobis@gmail.com>
2020-04-17 12:19:23 -0400

    fixed part 1

--- Project2/part1/mapperpt1.py
+++ Project2/part1/mapperpt1.py
@@ -7,7 +7,7 @@ for data in sys.stdin:
     data = data.lower()
     data = re.sub(r'[^\w\s]',"",data)
     #words is a list
-    words = new_string.split() 
+    words = data.split() 
     for word in words:
 
         print(word, "\t1")
